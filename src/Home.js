116ccca9834977eac0725b8d2d98d6270e2e123f
Md.Sami Ul Haque <samiulh4@gmail.com>
2023-08-05 21:26:05 +0600

    starting video 10:footer section

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from 'styled-components';
 import HeroSection from "./components/HeroSectio";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
 
 const Home = () =>{
    const data = {
       name : "Thapa Store"
    };
-   return <HeroSection myData={data}/>
+   return <>
+      <HeroSection myData={data}/>
+   <Services/>
+   <Trusted />
+   </>
 };
 
 const Wrapper = styled.section`
